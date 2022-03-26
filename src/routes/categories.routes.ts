f61cc85a308a72e5e4b8b2dfae51fe2ee88cbf7e
Alana Ragusa <alana.ragusarabello@gmail.com>
2022-03-26 20:14:25 -0300

    added dto in categoriesrepositoruy file

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -7,16 +7,6 @@ const categoriesRoutes = Router();
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  const category = new Category();
-
-  Object.assign(category, {
-    name,
-    description,
-    created_at: new Date(),
-  });
-
-  categories.push(category);
-
   return response.status(201).json({ category });
 });
 
