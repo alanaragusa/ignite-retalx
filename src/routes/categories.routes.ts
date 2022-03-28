04b729f30399f915da3ba322647c3de284cf4ff0
Alana Ragusa <alana.ragusarabello@gmail.com>
2022-03-28 18:48:23 -0300

    added createcategoryservice file

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,12 +1,15 @@
 import { Router } from "express";
 
 import { CategoriesRepository } from "../repositories/CategoriesRepository";
+import { CreateCategoryService } from "../services/CreateCategoryService";
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
+
+  const createCategoryService = new CreateCategoryService(categoriesRepository);
 
   return response.status(201).send();
 });
--- src/services/CreateCategoryService.ts
+++ src/services/CreateCategoryService.ts
@@ -1,17 +1,21 @@
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
+
 interface IRequest {
   name: string;
   description: string;
 }
 
 class CreateCategoryService {
-  execute({ name, description }: IRequest) {
-    const categoryAlreadyExists = categoriesRepository.findByName(name);
+  constructor(private categoriesRepository: CategoriesRepository) {}
+
+  execute({ name, description }: IRequest): void {
+    const categoryAlreadyExists = this.categoriesRepository.findByName(name);
 
     if (categoryAlreadyExists) {
       throw new Error("Category already exists!");
     }
 
-    categoriesRepository.create({ name, description });
+    this.categoriesRepository.create({ name, description });
   }
 }
 
